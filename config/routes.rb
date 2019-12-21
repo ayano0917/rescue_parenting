9974dc9f65aa855473e2a048112751b1e40129c0
ayano0917 <yuki1306ni@yahoo.co.jp>
2019-12-21 16:10:01 +0900

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'main/index'
+  devise_for :parents, controllers: {
+    sessions:      'parents/sessions',
+    passwords:     'parents/passwords',
+    registrations: 'parents/registrations'
+  }
+  devise_for :child_minders, controllers: {
+    sessions:      'child_minders/sessions',
+    passwords:     'child_minders/passwords',
+    registrations: 'child_minders/registrations'
+  }
+  root to: "main#index"
   get 'gardens' => 'gardens#index'
 
 end
