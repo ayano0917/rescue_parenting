78cccde59ec1fec5afc3f97537d4121c77fa3e1e
ayano0917 <yuki1306ni@yahoo.co.jp>
2020-01-26 23:39:32 +0900

    activehush_city

--- db/migrate/20191221060050_create_gardens.rb
+++ db/migrate/20191221060050_create_gardens.rb
@@ -1,12 +1,12 @@
 class CreateGardens < ActiveRecord::Migration[5.2]
   def change
     create_table :gardens do |t|
-      t.string :room_name, null: false
-      t.integer :management_id, null: false
-      t.integer :start_year, null: false
-      t.integer :start_month, null: false
-      t.integer :start_day, null: false
-      t.integer :postal_code, null: false
+      # t.string :room_name, null: false
+      # t.integer :management_id, null: false
+      # t.integer :start_year, null: false
+      # t.integer :start_month, null: false
+      # t.integer :start_day, null: false
+      t.string :postal_code, null: false
       t.integer :prefecture_id, null: false
       t.integer :city_id, null: false
       t.string :detail
