52bb19aa9fe93fb661f49c1457fd7a35c7675b06
ayano0917 <yuki1306ni@yahoo.co.jp>
2020-03-08 23:18:30 +0900

    model作成

---
+++ app/models/years_of_experience.rb
@@ -0,0 +1,11 @@
+class Years_of_experience < ActiveHash::Base
+  self.data = [
+      {id: 1, name: '1年'}, {id: 2, name: '2年'}, {id: 3, name: '3年'},
+      {id: 4, name: '4年'}, {id: 5, name: '５年'}, {id: 6, name: '６年'},
+      {id: 7, name: '7年'}, {id: 8, name: '8年'}, {id: 9, name: '9年'},
+      {id: 10, name: '10年'}, {id: 11, name: '11年'}, {id: 12, name: '12年'},
+      {id: 13, name: '13年'}, {id: 14, name: '14年'}, {id: 15, name: '15年'},
+      {id: 16, name: '16年'}, {id: 17, name: '17年'}, {id: 18, name: '18年'},
+      {id: 19, name: '19年'}, {id: 20, name: '20年以上'}
+  ]
+end
--- db/migrate/20191221060022_add_column_to_child_minder.rb
+++ db/migrate/20191221060022_add_column_to_child_minder.rb
@@ -1,9 +1,9 @@
 class AddColumnToChildMinder < ActiveRecord::Migration[5.2]
   def change
+    add_column :child_minders, :icon, :string
+    add_column :child_minders, :introduction, :string
     add_column :child_minders, :child_minder_nickname, :string
     add_column :child_minders, :gender_id, :integer
-    add_column :child_minders, :icon, :string
-    add_column :child_minders, :years_of_experience, :integer
-    add_column :child_minders, :introduction, :string
+    add_column :child_minders, :years_of_experience_id, :integer
   end
 end
